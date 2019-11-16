f3dd39ed945a3c00533c54f2d597bb8e9f2bc684
Brian <thatbrian@pop-os.localdomain>
2019-11-16 00:21:55 -0500

    Worked on a little of Profile

--- src/component/profile.component.js
+++ src/component/profile.component.js
@@ -6,8 +6,12 @@ import { Link } from "react-router-dom";
 export default class Profile extends Component {
     render() {
         return (
-            <div id="landing">
-                Profile
+            <div id="landing" class="profileLanding">
+                <div id="profile">
+                    <div>
+                        <div> Welcome, Rohit!</div>
+                    </div>
+                </div>
             </div>
         )
     }
