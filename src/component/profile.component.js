611262024999ad5ecaf4896b6c6b58b54aaf890c
Brian <thatbrian@pop-os.localdomain>
2019-11-15 19:10:29 -0500

    First commit

---
+++ src/component/profile.component.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <div>
+                Profile
+            </div>
+        )
+    }
+}
