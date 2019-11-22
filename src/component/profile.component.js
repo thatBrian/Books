dcbe80ad3657309de10efacab452ec54b98b9500
thatBrian <ye229@nyu.edu>
2019-11-21 22:44:20 -0500

    Updated Profile??

--- src/component/profile.component.js
+++ src/component/profile.component.js
@@ -7,9 +7,23 @@ export default class Profile extends Component {
     render() {
         return (
             <div id="landing" class="profileLanding">
-                <div id="profile">
-                    <div>
-                        <div> Welcome, Rohit!</div>
+                <div id="profilePadder">
+                    <div id="profile">
+                        <div className="row">
+                            <h1 id="profileWelcome"> Welcome, Rohit!</h1>
+                            <div></div>
+                        </div>
+                        <div className="row">
+                            <div id="profileBooks">
+                                <div>
+                                    Listing #1
+                                </div>
+                            </div>
+                            <div id="listings">
+                                <div >Create Listing</div>
+                                <div></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
